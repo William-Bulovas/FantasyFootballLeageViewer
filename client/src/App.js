3546f4cd008f82548d519c56b649b747eaf03007
William Bulovas <william.bulovas@uwaterloo.ca>
2018-04-20 22:38:48 -0400

    Remove react-bootstrap from the App.js Add bootstrap 4 to the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import { Navbar, NavItem, Nav, Button } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import ClientOAuth2 from 'client-oauth2';
 
@@ -9,8 +8,6 @@ import History from './pages/History';
 import Players from './pages/Players';
 
 import { connect } from 'react-redux';
-
-import './App.css';
 
 class App extends Component {
 	state = { page: 0 }	
@@ -33,51 +30,48 @@ class App extends Component {
 		
 		const { page } = this.state;
 
-		const rightStyle = {
-			position: "absolute",
-			right: "10px",
-		};
-
 		return (
 			<Router>
 				<div className="App">
 					<header className="App-header">
-						<h1 className="App-title">Throw up the X Fantasy Football League</h1>
-						{  this.props.isLoggedIn ? 
-							<div/>
-							:
-							<Button style={rightStyle} onClick={() => {
-									window.location.href = requestAuthURI;
-								}
-									}>
-								Login
-							</Button>
-						}
+						<nav collapseOnSelect className='navbar navbar-expand navbar-dark bg-dark'>
+							<div class="container">
+								<a className='navbar-brand' href='/'>Throw up the X Fantasy Football League</a>
+								<div className='collapse navbar-collapse'>
+									<ul className='navbar-nav mr-auto'>
+										<li className='nav-item'>
+											<a className='nav-link' componentClass={Link} href='/' to='/'>Home</a>
+										</li>
+										<li className='nav-item'>
+											<a className='nav-link' componentClass={Link} href='/history' to='/history'>History</a>
+										</li>
+										<li className='nav-item'>
+											<a className='nav-link' componentClass={Link} href='/players' to='/players'>Players</a>
+										</li>
+									</ul>
+									<button className='btn btn-outline-primary form-inline my-2 my-md-0' onClick={() => {
+											window.location.href = requestAuthURI;
+										}}>
+										Login
+									</button>
+								</div>
+							</div>
+						</nav>						
 					</header>
-					<Navbar collapseOnSelect>
-						<Navbar.Header>
-							<Navbar.Toggle />
-						</Navbar.Header>
-						<Navbar.Collapse>
-							<Nav>
-								<NavItem componentClass={Link} href='/' to='/'>Home</NavItem>
-								<NavItem componentClass={Link} href='/history'to='/history'>History</NavItem>
-								<NavItem componentClass={Link} href='/players'to='/players'>Players</NavItem>
-							</Nav>
-						</Navbar.Collapse>
-					</Navbar>
-					<Route exact path="/" component={Home}/>
-					<Route path="/history" component={History}/>
-					<Route path="/players" component={Players}/>
-					<Route path="/auth/yahoo/:query" component={
-						({match}) => {
-							this.props.dispatch({
-								type: "LOGGED_IN",
-								token: this.getJsonFromUrl(window.location.href)['https://fantasyfootballviewer.herokuapp.com/auth/yahoo/callback#access_token'],
-							});
-							return <Redirect to='/' />
-						}
-					}/>
+					<div class="container">
+						<Route exact path="/" component={Home}/>
+						<Route path="/history" component={History}/>
+						<Route path="/players" component={Players}/>
+						<Route path="/auth/yahoo/:query" component={
+							({match}) => {
+								this.props.dispatch({
+									type: "LOGGED_IN",
+									token: this.getJsonFromUrl(window.location.href)['https://fantasyfootballviewer.herokuapp.com/auth/yahoo/callback#access_token'],
+								});
+								return <Redirect to='/' />
+							}
+						}/>
+					</div>
 				</div>
 			</Router>
 		);
