ff249bb8a2d840e56d336bd036951141d09eb539
William Bulovas <william.bulovas@uwaterloo.ca>
2018-06-03 14:56:51 -0400

    Add matchups call to redux

--- client/src/reducers/FantasyFootballReducer.js
+++ client/src/reducers/FantasyFootballReducer.js
@@ -1,102 +1,107 @@
 import {
-    REQUEST_CUREENT_STANDINGS,
-    RECEIVE_CURRENT_STANDINGS
-} from './CurrentStandingsActions'
+  REQUEST_CUREENT_STANDINGS,
+  RECEIVE_CURRENT_STANDINGS
+} from "./CurrentStandingsActions";
 
-import {
-    REQUEST_CAREER,
-    RECEIVE_CAREER
-} from './CareerActions'
+import { REQUEST_CAREER, RECEIVE_CAREER } from "./CareerActions";
 
-import {
-    REQUEST_STANDINGS,
-    RECEIVE_STANDINGS
-} from './StandingsActions'
+import { REQUEST_STANDINGS, RECEIVE_STANDINGS } from "./StandingsActions";
 
-import {
-    REQUEST_WEEK_STATS,
-    RECEIVE_WEEK_STATS
-} from './WeekStatsActions'
+import { REQUEST_WEEK_STATS, RECEIVE_WEEK_STATS } from "./WeekStatsActions";
+
+import { REQUEST_MATCHUPS, RECEIVE_MATCHUPS } from "./MatchupActions";
 
 export const LOGGED_IN = "LOGGED_IN";
 
 const inialState = {
-    isLoggedIn: false,
-    access_token: null,
-    currentStandings: null,
-    lastUpdatedCurrent: null,
-    career: null,
-    standings: null,
-    weekRoster: {},
-}
+  isLoggedIn: false,
+  access_token: null,
+  currentStandings: null,
+  lastUpdatedCurrent: null,
+  career: null,
+  standings: null,
+  weekRoster: {},
+  matchups: {}
+};
 
-export default function FantasyFootballReducer(state = inialState, action){
-    if (state.isLoggedIn) {
-        if (state.expires_in - Date.now() <= 0){
-            console.log("In check");
-            
-            return Object.assign({}, state, {
-                isLoggedIn : false
-            });
-        }
+export default function FantasyFootballReducer(state = inialState, action) {
+  if (state.isLoggedIn) {
+    if (state.expires_in - Date.now() <= 0) {
+      console.log("In check");
+
+      return Object.assign({}, state, {
+        isLoggedIn: false
+      });
     }
-    switch(action.type){
-        case LOGGED_IN:
-            const expires = action.expires_in * 1000 + Date.now();
-            return Object.assign({}, state, {
-                isLoggedIn: true,
-                access_token: action.token,
-                expires_in: expires
-            })
-        case REQUEST_CUREENT_STANDINGS:
-            return Object.assign({}, state, {
-                isFetchingCurrent: true
-            })
-        case RECEIVE_CURRENT_STANDINGS:
-            return Object.assign({}, state, {
-                isFetchingCurrent: false,
-                lastUpdatedCurrent: action.receivedAt,
-                currentStandings: action.standings,
-            })
-        case REQUEST_CAREER:
-            return Object.assign({}, state, {
-                isFetchingCareer: true
-            })
-        case RECEIVE_CAREER:
-            return Object.assign({}, state, {
-                isFetchingCareer: false,
-                lastUpdatedCareer: action.receivedAt,
-                career: action.career,
-            })
-        case REQUEST_STANDINGS:
-            return Object.assign({}, state, {
-                isFetchingStandings: true
-            })
-        case RECEIVE_STANDINGS:
-            return Object.assign({}, state, {
-                isFetchingStandings: false,
-                lastUpdatedStandings: action.receivedAt,
-                standings: action.standings,
-            })
-        case REQUEST_WEEK_STATS:
-            return Object.assign({}, state, {
-                isFetchingWeekStats: true
-            })
-        case RECEIVE_WEEK_STATS:
-            const roster = state.weekRoster;
+  }
+  switch (action.type) {
+    case LOGGED_IN:
+      const expires = action.expires_in * 1000 + Date.now();
+      return Object.assign({}, state, {
+        isLoggedIn: true,
+        access_token: action.token,
+        expires_in: expires
+      });
+    case REQUEST_CUREENT_STANDINGS:
+      return Object.assign({}, state, {
+        isFetchingCurrent: true
+      });
+    case RECEIVE_CURRENT_STANDINGS:
+      return Object.assign({}, state, {
+        isFetchingCurrent: false,
+        lastUpdatedCurrent: action.receivedAt,
+        currentStandings: action.standings
+      });
+    case REQUEST_CAREER:
+      return Object.assign({}, state, {
+        isFetchingCareer: true
+      });
+    case RECEIVE_CAREER:
+      return Object.assign({}, state, {
+        isFetchingCareer: false,
+        lastUpdatedCareer: action.receivedAt,
+        career: action.career
+      });
+    case REQUEST_STANDINGS:
+      return Object.assign({}, state, {
+        isFetchingStandings: true
+      });
+    case RECEIVE_STANDINGS:
+      return Object.assign({}, state, {
+        isFetchingStandings: false,
+        lastUpdatedStandings: action.receivedAt,
+        standings: action.standings
+      });
+    case REQUEST_WEEK_STATS:
+      return Object.assign({}, state, {
+        isFetchingWeekStats: true
+      });
+    case RECEIVE_WEEK_STATS:
+      const roster = state.weekRoster;
 
-            if (roster[action.teamId] === undefined) {
-                roster[action.teamId] = {};
-            }
-            
-            roster[action.teamId][action.week] = action.weekRoster;
-            return Object.assign({}, state, {
-                isFetchingWeekStats: false,
-                lastUpdatedWeekStats: action.receivedAt,
-                weekRoster: roster,
-            })
+      if (roster[action.teamId] === undefined) {
+        roster[action.teamId] = {};
+      }
 
-        default:
-            return state
-    }
+      roster[action.teamId][action.week] = action.weekRoster;
+      return Object.assign({}, state, {
+        isFetchingWeekStats: false,
+        lastUpdatedWeekStats: action.receivedAt,
+        weekRoster: roster
+      });
+    case REQUEST_MATCHUPS:
+      return Object.assign({}, state, {
+        isFetchingMatchups: true
+      });
+    case RECEIVE_MATCHUPS:
+      const matchups = state.matchups;
+
+      matchups[action.teamId] = action.matchups;
+
+      return Object.assign({}, state, {
+        matchups: matchups
+      });
+    default:
+      return state;
+  }
 }
---
+++ client/src/reducers/MatchupActions.js
@@ -0,0 +1,48 @@
+export const REQUEST_MATCHUPS = "REQUEST_MATCHUPS";
+export const RECEIVE_MATCHUPS = "RECEIVE_MATCHUPS";
+
+function requestMatchups(teamId) {
+  return {
+    type: REQUEST_MATCHUPS,
+    teamId: teamId
+  };
+}
+
+function receiveMatchups(matchups, teamId) {
+  return {
+    type: RECEIVE_MATCHUPS,
+    matchups: matchups,
+    receivedAt: Date.now(),
+    teamId: teamId
+  };
+}
+
+function fetchMatchups(state, teamId) {
+  return dispatch => {
+    dispatch(requestMatchups(teamId));
+    return fetch(
+      "api/league/teams/matchups/" + teamId + "/" + state.access_token
+    )
+      .then(response => response.json())
+      .then(json => dispatch(receiveMatchups(json, teamId)));
+  };
+}
+
+function shouldFetchMatchups(state, teamId) {
+  if (!state.isLoggedIn) {
+    return false;
+  }
+  if (!state.weekRoster.hasOwnProperty(teamId)) {
+    return true;
+  }
+
+  return false;
+}
+
+export function fetchMatchupsIfNeeded(teamId, week) {
+  return (dispatch, getState) => {
+    if (shouldFetchMatchups(getState(), teamId, week)) {
+      return dispatch(fetchMatchups(getState(), teamId, week));
+    }
+  };
+}
